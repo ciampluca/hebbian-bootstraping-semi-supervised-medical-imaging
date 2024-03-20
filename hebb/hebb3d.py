f9dac68f14bda6f3f6a40a3f350358d089fd6e48
Gabriele Lagani <gabriele.lagani@gmail.com>
2024-03-20 17:05:29 +0100

    Added Hebbian 3d modules

---
+++ hebb/hebb3d.py
@@ -0,0 +1,271 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.utils import _triple
+
+
+def normalize(x, dim=None):
+	nrm = (x**2).sum(dim=dim, keepdim=True)**0.5
+	nrm[nrm == 0] = 1.
+	return x / nrm
+
+
+class HebbianConv3d(nn.Module):
+	"""
+	A 2d convolutional layer that learns through Hebbian plasticity
+	"""
+	
+	MODE_SWTA = 'swta'
+	MODE_HPCA = 'hpca'
+	MODE_CONTRASTIVE = 'contrastive'
+
+	def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True,
+	             w_nrm=True, act=nn.Identity(),
+	             mode=MODE_SWTA, k=1, patchwise=True,
+	             contrast=1., uniformity=False, alpha=0.):
+		"""
+
+		:param out_channels: output channels of the convolutional kernel
+		:param in_channels: input channels of the convolutional kernel
+		:param kernel_size: size of the convolutional kernel (int or tuple)
+		:param stride: stride of the convolutional kernel (int or tuple)
+		:param stride: padding to apply before convolution (int or tuple)
+		:param w_nrm: whether to normalize the weight vectors before computing outputs
+		:param act: the nonlinear activation function after convolution
+		:param mode: the learning mode, either 'swta' or 'hpca'
+		:param k: softmax inverse temperature parameter used for swta-type learning
+		:param patchwise: whether updates for each convolutional patch should be computed separately,
+		and then aggregated
+		:param contrast: coefficient that rescales negative compared to positive updates in contrastive-type learning
+		:param uniformity: whether to use uniformity weighting in contrastive-type learning.
+		:param alpha: weighting coefficient between hebbian and backprop updates (0 means fully backprop, 1 means fully hebbian).
+		
+		"""
+		
+		super().__init__()
+		self.mode = mode
+		self.out_channels = out_channels
+		self.in_channels = in_channels
+		self.kernel_size = _triple(kernel_size)
+		self.padding = padding
+		self.stride = _triple(stride)
+
+		self.weight = nn.Parameter(torch.empty((self.out_channels, self.in_channels, *self.kernel_size)), requires_grad=True)
+		nn.init.xavier_normal_(self.weight)
+		self.w_nrm = w_nrm
+		self.bias = nn.Parameter(torch.zeros(self.out_channels), requires_grad=bias)
+		self.act = act
+		self.register_buffer('delta_w', torch.zeros_like(self.weight))
+
+		self.k = k
+		self.patchwise = patchwise
+		self.contrast = contrast
+		self.uniformity = uniformity
+		
+		self.alpha = alpha
+
+	def apply_weights(self, x, w):
+		"""
+		This function provides the logic for combining input x and weight w
+		"""
+		
+		return torch.conv3d(x, w, bias=self.bias, stride=self.stride)
+	
+	def compute_activation(self, x):
+		w = self.weight
+		if self.w_nrm: w = normalize(w, dim=(1, 2, 3, 4))
+		y = self.act(self.apply_weights(x, w))
+		return y
+	
+	def pad(self, x):
+		pad = [self.padding]*6 if isinstance(self.padding, int) else [self.padding[0], self.padding[0], self.padding[1], self.padding[1], self.padding[2], self.padding[2]] if len(self.padding) == 3 else self.padding
+		return F.pad(x, pad)
+	
+	def forward(self, x):
+		x = self.pad(x)
+		y = self.compute_activation(x)
+		if self.training and self.alpha != 0: self.compute_update(x, y)
+		return y
+	
+	def unfold3d(self, x, kernel_size, stride=(0, 0, 0), padding=0):
+		x = F.pad(x, [padding]*6)
+		B, C, D, H, W = x.shape
+		x_unf = F.unfold(x.reshape(B, C*D, H, W), kernel_size=kernel_size[-2:], stride=stride[-2:])
+		x_unf = x_unf.reshape(B, C, D, -1, x_unf.shape[-1])
+		K, L = x_unf.shape[3], x_unf.shape[4]
+		x_unf = x_unf.permute(0, 4, 1, 3, 2).reshape(B, L, C, K, D)
+		x_unf = x_unf.unfold(4, kernel_size[0], stride[0])
+		x_unf = x_unf.permute(0, 2, 4, 3, 5, 1).reshape(B, -1, x_unf.shape[-1]*L)
+		return x_unf
+	
+	def compute_update(self, x, y):
+		"""
+		This function implements the logic that computes local plasticity rules from input x and output y. The
+		resulting weight update is stored in buffer self.delta_w for later use.
+		"""
+		
+		if self.mode not in [self.MODE_SWTA, self.MODE_HPCA, self.MODE_CONTRASTIVE]:
+			raise NotImplementedError("Learning mode {} unavailable for {} layer".format(self.mode, self.__class__.__name__))
+		
+		if self.mode == self.MODE_SWTA:
+			with torch.no_grad():
+				# Logic for swta-type learning
+				x_unf = self.unfold3d(x, self.kernel_size, self.stride)
+				x_unf = x_unf.permute(0, 2, 1).reshape(-1, x_unf.size(1))
+				if self.patchwise:
+					r = (y * self.k).softmax(dim=1).permute(1, 0, 2, 3, 4).reshape(y.shape[1], -1)
+					dec = r.sum(1, keepdim=True) * self.weight.reshape(self.weight.shape[0], -1)
+					self.delta_w += (r.matmul(x_unf) - dec).reshape_as(self.weight)
+				else:
+					r = (y * self.k).softmax(dim=1).permute(2, 3, 4, 1, 0)
+					krn = torch.eye(len(self.weight[0]), device=x.device, dtype=x.dtype).view(len(self.weight[0]), self.weight.shape[1], *self.kernel_size)
+					dec = torch.conv_transpose3d((r.sum(dim=-1, keepdim=True) * self.weight.reshape(1, 1, 1, self.weight.shape[0], -1)).permute(3, 4, 0, 1, 2), krn, stride=self.stride)
+					self.delta_w += (r.permute(3, 4, 0, 1, 2).reshape(r.shape[3], -1).matmul(x_unf) - self.unfold3d(dec, self.kernel_size, self.stride).sum(dim=-1)).reshape_as(self.weight)
+			
+		if self.mode == self.MODE_HPCA:
+			with torch.no_grad():
+				# Logic for hpca-type learning
+				x_unf = self.unfold3d(x, self.kernel_size, self.stride)
+				x_unf = x_unf.permute(0, 2, 1).reshape(-1, x_unf.size(1))
+				if self.patchwise:
+					r = y.permute(1, 0, 2, 3, 4).reshape(y.shape[1], -1)
+					l = (torch.arange(self.weight.shape[0], device=x.device, dtype=x.dtype).unsqueeze(0).repeat(self.weight.shape[0], 1) <= torch.arange(self.weight.shape[0], device=x.device, dtype=x.dtype).unsqueeze(1)).to(dtype=x.dtype)
+					dec = (r.matmul(r.transpose(-2, -1)) * l).matmul(self.weight.reshape(self.weight.shape[0], -1))
+					self.delta_w += (r.matmul(x_unf) - dec).reshape_as(self.weight)
+				else:
+					r = y.permute(2, 3, 4, 1, 0)
+					l = (torch.arange(self.weight.shape[0], device=x.device, dtype=x.dtype).unsqueeze(0).repeat(self.weight.shape[0], 1) <= torch.arange(self.weight.shape[0], device=x.device, dtype=x.dtype).unsqueeze(1)).to(dtype=x.dtype)
+					dec = torch.conv_transpose3d((r.matmul(r.transpose(-2, -1)) * l.unsqueeze(0).unsqueeze(1).unsqueeze(2)).permute(4, 3, 0, 1, 2), self.weight, stride=self.stride)
+					self.delta_w += (r.permute(3, 4, 0, 1, 2).reshape(r.shape[3], -1).matmul(x_unf) - self.unfold3d(dec, self.kernel_size, self.stride).sum(dim=-1)).reshape_as(self.weight)
+			
+		if self.mode == self.MODE_CONTRASTIVE:
+			y = self.compute_activation(x.clone().detach())
+			y = normalize(y, dim=1)
+			y_unf = self.unfold3d(y, _triple(3), padding=_triple(1))
+			y_unf = y_unf.permute(0, 2, 1).reshape(y_unf.size(0), y_unf.size(2), y.size(1), 27)
+			
+			# Positive contribution
+			L = - y_unf.sum(-1).reshape(-1, y.size(1)) * y.permute(0, 2, 3, 4, 1).reshape(-1, y.size(1))
+			
+			# Uniformity weighting, if required
+			if self.uniformity:
+				with torch.no_grad():
+					x = normalize(x, dim=1)
+					x_unf = self.unfold3d(x, _triple(3), padding=_triple(1))
+					x_unf = x_unf.permute(0, 2, 1).reshape(x_unf.size(0), x_unf.size(2), x.size(1), 27)
+					uniformity_map = (x_unf.sum(-1).reshape(-1, x.size(1)) * x.permute(0, 2, 3, 4, 1).reshape(-1, x.size(1))).sum(dim=-1, keepdim=True)
+					uniformity_map = self.apply_weights(uniformity_map.reshape(x.size(0), 1, *x.shape[2:]), torch.ones([1, 1, *self.kernel_size], device=x.device, dtype=x.dtype)).reshape(-1, 1)
+				L = L * uniformity_map
+			
+			# Negative contribution
+			idx = torch.randperm(y_unf.size(0), device=y_unf.device)
+			L = L + self.contrast * y_unf[idx].sum(-1).reshape(-1, y.size(1)) * y.permute(0, 2, 3, 4, 1).reshape(-1, y.size(1))
+			
+			# Compute update
+			L = L.sum()
+			prev_grad = self.weight.grad
+			self.weight.grad = None
+			L.backward()
+			self.delta_w += self.weight.grad.clone().detach()
+			self.weight.grad = prev_grad
+	
+	@torch.no_grad()
+	def local_update(self):
+		"""
+		
+		This function transfers a previously computed weight update, stored in buffer self.delta_w, to the gradient
+		self.weight.grad of the weight parameter.
+		
+		This function should be called before optimizer.step(), so that the optimizer will use the locally computed
+		update as optimization direction. Local updates can also be combined with end-to-end updates by calling this
+		function between loss.backward() and optimizer.step(). loss.backward will store the end-to-end gradient in
+		self.weight.grad, and this function combines this value with self.delta_w as
+		self.weight.grad = (1 - alpha) * self.weight.grad - alpha * self.delta_w
+		Parameter alpha determines the scale of the local update compared to the end-to-end gradient in the combination.
+		
+		"""
+		
+		if self.weight.grad is None: self.weight.grad = -self.alpha * self.delta_w
+		else: self.weight.grad = (1 - self.alpha) * self.weight.grad - self.alpha * self.delta_w
+		self.delta_w.zero_()
+
+
+class HebbianConvTranspose3d(HebbianConv3d):
+	"""
+	A 2d convolutional layer that learns through Hebbian plasticity
+	"""
+	
+	MODE_SWTA_T = 'swta_t'
+	MODE_HPCA_T = 'hpca_t'
+
+	def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True, w_nrm=True, act=nn.Identity(),
+	             mode=MODE_SWTA_T, k=1, patchwise=False, contrast=1., uniformity=False, alpha=0.):
+		"""
+		
+		:param out_channels: output channels of the convolutional kernel
+		:param in_channels: input channels of the convolutional kernel
+		:param kernel_size: size of the convolutional kernel (int or tuple)
+		:param stride: stride of the convolutional kernel (int or tuple)
+		:param w_nrm: whether to normalize the weight vectors before computing outputs
+		:param act: the nonlinear activation function after convolution
+		:param mode: the learning mode, either 'swta', 'swta_t, 'hpca', 'hpca_t', or 'contrastive'
+		:param k: softmax inverse temperature parameter used for swta-type learning
+		:param patchwise: whether updates for each convolutional patch should be computed separately,
+		and then aggregated
+		:param contrast: coefficient that rescales negative compared to positive updates in contrastive-type learning
+		:param uniformity: whether to use uniformity weighting in contrastive-type learning.
+		"""
+		
+		super().__init__(in_channels, out_channels, kernel_size, stride, padding, bias, w_nrm, act, mode, k, patchwise, contrast, uniformity, alpha)
+		with torch.no_grad():
+			self.weight.transpose_(0, 1)
+			self.delta_w.transpose_(0, 1)
+
+	def apply_weights(self, x, w):
+		"""
+		This function implements the logic that computes Hebbian plasticity rules from input x and output y. The
+		resulting weight update is stored in buffer self.delta_w for later use.
+		"""
+		
+		return torch.conv_transpose3d(x, w, bias=self.bias, stride=self.stride)
+
+	def compute_update(self, x, y):
+		"""
+		This function implements the logic that computes local plasticity rules from input x and output y. The
+		resulting weight update is stored in buffer self.delta_w for later use.
+		"""
+		
+		if self.mode not in [self.MODE_SWTA, self.MODE_HPCA, self.MODE_SWTA_T, self.MODE_HPCA_T, self.MODE_CONTRASTIVE]:
+			raise NotImplementedError("Learning mode {} unavailable for {} layer".format(self.mode, self.__class__.__name__))
+		
+		if self.mode in [self.MODE_SWTA, self.MODE_HPCA]:
+			# In case of swta-type or hpca-type learning, use the learning rules for ordinary convolution,
+			# but exchanging x and y
+			super().compute_update(y, x)
+		
+		if self.mode in [self.MODE_CONTRASTIVE]:
+			# Reuse update from base conv class
+			super().compute_update(x, y)
+		
+		if self.mode == self.MODE_SWTA_T:
+			with torch.no_grad():
+				# Logic for swta-type learning in transpose convolutional layers
+				r = (y * self.k).softmax(dim=1)
+				r = self.unfold3d(r, kernel_size=self.kernel_size, stride=self.stride)
+				r = r.permute(0, 2, 1).reshape(-1, self.out_channels, self.kernel_size[0]*self.kernel_size[1]*self.kernel_size[2]).permute(2, 1, 0)
+				dec = r.sum(2, keepdim=True) * self.weight.permute(2, 3, 4, 1, 0).reshape(-1, self.out_channels, self.in_channels)
+				if self.patchwise: dec = dec.sum(dim=0, keepdim=True)
+				self.delta_w += (r.matmul(x.permute(0, 2, 3, 4, 1).reshape(1, -1, x.size(1))) - dec).permute(2, 1, 0).reshape_as(self.weight)
+		
+		if self.mode == self.MODE_HPCA_T:
+			with torch.no_grad():
+				# Logic for hpca-type learning in transpose convolutional layers
+				r = y
+				r = self.unfold3d(r, kernel_size=self.kernel_size, stride=self.stride)
+				r = r.permute(0, 2, 1).reshape(-1, self.out_channels, self.kernel_size[0]*self.kernel_size[1]*self.kernel_size[2]).permute(2, 1, 0)
+				l = (torch.arange(self.out_channels, device=x.device, dtype=x.dtype).unsqueeze(0).repeat(self.out_channels, 1) <= torch.arange(self.out_channels, device=x.device, dtype=x.dtype).unsqueeze(1)).to(dtype=x.dtype)
+				dec = (r.matmul(r.transpose(-2, -1)) * l.unsqueeze(0)).matmul(self.weight.permute(2, 3, 4, 1, 0).reshape(-1, self.out_channels, self.in_channels))
+				if self.patchwise: dec = dec.sum(dim=0, keepdim=True)
+				self.delta_w += (r.matmul(x.permute(0, 2, 3, 4, 1).reshape(1, -1, x.size(1))) - dec).permute(2, 1, 0).reshape_as(self.weight)
+		
+		
--- hebb/makehebbian.py
+++ hebb/makehebbian.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 from .hebb import HebbianConv2d, HebbianConvTranspose2d
+from .hebb3d import HebbianConv3d, HebbianConvTranspose3d
 
 
-pad_layers = {'zeros': nn.ZeroPad2d, 'reflect': nn.ReflectionPad2d, 'replicate': nn.ReplicationPad2d,'circular': nn.CircularPad2d if hasattr(nn, 'CircularPad2d') else None}
 default_hebb_params = {'w_nrm': True, 'act': nn.Identity(), 'mode': HebbianConvTranspose2d.MODE_SWTA_T, 'k': 50, 'patchwise': True, 'contrast': 1., 'uniformity': False, 'alpha': 0.}
 
 class UnsqueezeLast(nn.Module):
@@ -54,36 +54,33 @@ def makehebbian(model, exclude=None, hebb_params=None):
     print("Layers excluded from conversion to Hebbian: {}".format([n for n, m in exclude]))
     exclude = [m for n, p in exclude for m in [*p.modules()]]
     
+    def _replacelayer(m, n, l):
+        #setattr(m, n, l)
+        m.register_module(n, l)
+    
     def _makehebbian(module):
         for n, m in module.named_children():
             if m in exclude: continue
             if type(m) is nn.Conv2d:
                 if m.dilation != 1 and m.dilation != (1, 1): raise RuntimeError("Dilation not supported with Hebbian layers")
                 if m.groups != 1: raise RuntimeError("Grouped convolution not supported with Hebbian layers")
-                PadLayer = pad_layers.get(m.padding_mode, None)
-                if PadLayer is None: raise RuntimeError("Padding mode {} unsupported".format(m.padding_mode))
-                setattr(module, n, nn.Sequential(
-                    PadLayer(m.padding if isinstance(m.padding, int) else (m.padding[0], m.padding[0], m.padding[1], m.padding[1])),
-                    init_weights(HebbianConv2d(m.in_channels, m.out_channels, m.kernel_size, m.stride, **adjust_hebbian_params(hebb_params))),
-                ))
+                _replacelayer(module, n, init_weights(HebbianConv2d(m.in_channels, m.out_channels, m.kernel_size, m.stride, m.padding, False, **adjust_hebbian_params(hebb_params)), init_type='kaiming'))
             elif type(m) is nn.ConvTranspose2d:
                 if m.dilation != 1 and m.dilation != (1, 1): raise RuntimeError("Dilation not supported with Hebbian layers")
                 if m.groups != 1: raise RuntimeError("Grouped convolution not supported with Hebbian layers")
-                PadLayer = pad_layers.get(m.padding_mode, None)
-                if PadLayer is None: raise RuntimeError("Padding mode {} unsupported".format(m.padding_mode))
-                setattr(module, n, nn.Sequential(
-                    PadLayer(m.padding if isinstance(m.padding, int) else (m.padding[0], m.padding[0], m.padding[1], m.padding[1])),
-                    init_weights(HebbianConvTranspose2d(m.in_channels, m.out_channels, m.kernel_size, m.stride, **hebb_params)),
-                    PadLayer((0, m.output_padding, 0, m.output_padding) if isinstance(m.output_padding, int) else (0, m.output_padding[0], 0, m.output_padding[1])),
-                ))
+                _replacelayer(module, n, init_weights(HebbianConvTranspose2d(m.in_channels, m.out_channels, m.kernel_size, m.stride, m.padding, False, **hebb_params), init_type='kaiming'))
+            if type(m) is nn.Conv3d:
+                if m.dilation != 1 and m.dilation != (1, 1, 1): raise RuntimeError("Dilation not supported with Hebbian layers")
+                if m.groups != 1: raise RuntimeError("Grouped convolution not supported with Hebbian layers")
+                _replacelayer(module, n, init_weights(HebbianConv3d(m.in_channels, m.out_channels, m.kernel_size, m.stride, m.padding, False, **adjust_hebbian_params(hebb_params)), init_type='kaiming'))
+            elif type(m) is nn.ConvTranspose3d:
+                if m.dilation != 1 and m.dilation != (1, 1, 1): raise RuntimeError("Dilation not supported with Hebbian layers")
+                if m.groups != 1: raise RuntimeError("Grouped convolution not supported with Hebbian layers")
+                _replacelayer(module, n, init_weights(HebbianConvTranspose3d(m.in_channels, m.out_channels, m.kernel_size, m.stride, m.padding, False, **hebb_params), init_type='kaiming'))
             elif type(m) is nn.Linear:
-                setattr(module, n, nn.Sequential(
-                    UnsqueezeLast(2),
-                    init_weights(HebbianConv2d(m.in_features, m.out_features, 1, 1, **adjust_hebbian_params(hebb_params))),
-                    FlattenLast(2),
-                ))
+                _replacelayer(module, n, nn.Sequential(UnsqueezeLast(2), init_weights(HebbianConv2d(m.in_features, m.out_features, 1, 1, **adjust_hebbian_params(hebb_params)), init_type='kaiming'), FlattenLast(2)))
             else:
-                for p in m.parameters(): p.requires_grad = False
+                for p in m.parameters(recurse=False): p.requires_grad = False
     
     model.apply(_makehebbian)
 

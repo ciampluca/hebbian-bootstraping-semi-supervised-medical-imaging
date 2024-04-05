3624719e87d0573e34a1e244530234f49ddbf260
luca-cnr <luca.ciampi@isti.cnr.it>
2024-04-05 11:12:24 +0200

    added unet2d trans

--- models/networks_2d/unet.py
+++ models/networks_2d/unet.py
@@ -28,9 +28,9 @@ def init_weights(net, init_type='normal', gain=0.02):
     net.apply(init_func)
 
 
-class conv_block(nn.Module):
-    def __init__(self, ch_in, ch_out):
-        super(conv_block, self).__init__()
+class ConvBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, dropout=None):
+        super(ConvBlock, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
             nn.BatchNorm2d(ch_out),
@@ -43,6 +43,25 @@ class conv_block(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         return x
+    
+
+class ConvBlockLeakyRelu(nn.Module):
+    """two convolution layers with batch norm and leaky relu"""
+
+    def __init__(self, in_channels, out_channels, dropout_p):
+        super(ConvBlock, self).__init__()
+        self.conv_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_p),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU()
+        )
+
+    def forward(self, x):
+        return self.conv_conv(x)
 
 
 class up_conv(nn.Module):
@@ -138,6 +157,97 @@ class Attention_block(nn.Module):
         psi = self.psi(psi)
 
         return x * psi
+    
+
+class UpBlock(nn.Module):
+    """Upssampling followed by ConvBlock"""
+
+    def __init__(self, in_channels1, in_channels2, out_channels, dropout_p,
+                 bilinear=True):
+        super(UpBlock, self).__init__()
+        self.bilinear = bilinear
+        if bilinear:
+            self.conv1x1 = nn.Conv2d(in_channels1, in_channels2, kernel_size=1)
+            self.up = nn.Upsample(
+                scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(
+                in_channels1, in_channels2, kernel_size=2, stride=2)
+        self.conv = ConvBlock(in_channels2 * 2, out_channels, dropout_p)
+
+    def forward(self, x1, x2):
+        if self.bilinear:
+            x1 = self.conv1x1(x1)
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, params):
+        super(Encoder, self).__init__()
+        self.params = params
+        self.in_chns = self.params['in_chns']
+        self.ft_chns = self.params['feature_chns']
+        self.n_class = self.params['class_num']
+        self.bilinear = self.params['bilinear']
+        self.dropout = self.params['dropout']
+        assert (len(self.ft_chns) == 5)
+        self.in_conv = ConvBlock(
+            self.in_chns, self.ft_chns[0], self.dropout[0])
+        self.down1 = DownBlock(
+            self.ft_chns[0], self.ft_chns[1], self.dropout[1])
+        self.down2 = DownBlock(
+            self.ft_chns[1], self.ft_chns[2], self.dropout[2])
+        self.down3 = DownBlock(
+            self.ft_chns[2], self.ft_chns[3], self.dropout[3])
+        self.down4 = DownBlock(
+            self.ft_chns[3], self.ft_chns[4], self.dropout[4])
+
+    def forward(self, x):
+        x0 = self.in_conv(x)
+        x1 = self.down1(x0)
+        x2 = self.down2(x1)
+        x3 = self.down3(x2)
+        x4 = self.down4(x3)
+        return [x0, x1, x2, x3, x4]
+
+
+class Decoder(nn.Module):
+    def __init__(self, params):
+        super(Decoder, self).__init__()
+        self.params = params
+        self.in_chns = self.params['in_chns']
+        self.ft_chns = self.params['feature_chns']
+        self.n_class = self.params['class_num']
+        self.bilinear = self.params['bilinear']
+        assert (len(self.ft_chns) == 5)
+
+        self.up1 = UpBlock(
+            self.ft_chns[4], self.ft_chns[3], self.ft_chns[3], dropout_p=0.0, bilinear=self.bilinear)
+        self.up2 = UpBlock(
+            self.ft_chns[3], self.ft_chns[2], self.ft_chns[2], dropout_p=0.0, bilinear=self.bilinear)
+        self.up3 = UpBlock(
+            self.ft_chns[2], self.ft_chns[1], self.ft_chns[1], dropout_p=0.0, bilinear=self.bilinear)
+        self.up4 = UpBlock(
+            self.ft_chns[1], self.ft_chns[0], self.ft_chns[0], dropout_p=0.0, bilinear=self.bilinear)
+
+        self.out_conv = nn.Conv2d(self.ft_chns[0], self.n_class,
+                                  kernel_size=3, padding=1)
+
+    def forward(self, feature):
+        x0 = feature[0]
+        x1 = feature[1]
+        x2 = feature[2]
+        x3 = feature[3]
+        x4 = feature[4]
+
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+        x = self.up4(x, x0)
+        output = self.out_conv(x)
+        return output
 
 
 class U_Net(nn.Module):
@@ -146,23 +256,23 @@ class U_Net(nn.Module):
 
         self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.Conv1 = conv_block(ch_in=in_channels, ch_out=64)
-        self.Conv2 = conv_block(ch_in=64, ch_out=128)
-        self.Conv3 = conv_block(ch_in=128, ch_out=256)
-        self.Conv4 = conv_block(ch_in=256, ch_out=512)
-        self.Conv5 = conv_block(ch_in=512, ch_out=1024)
+        self.Conv1 = ConvBlock(ch_in=in_channels, ch_out=64)
+        self.Conv2 = ConvBlock(ch_in=64, ch_out=128)
+        self.Conv3 = ConvBlock(ch_in=128, ch_out=256)
+        self.Conv4 = ConvBlock(ch_in=256, ch_out=512)
+        self.Conv5 = ConvBlock(ch_in=512, ch_out=1024)
 
         self.Up5 = up_conv(ch_in=1024, ch_out=512)
-        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
+        self.Up_conv5 = ConvBlock(ch_in=1024, ch_out=512)
 
         self.Up4 = up_conv(ch_in=512, ch_out=256)
-        self.Up_conv4 = conv_block(ch_in=512, ch_out=256)
+        self.Up_conv4 = ConvBlock(ch_in=512, ch_out=256)
 
         self.Up3 = up_conv(ch_in=256, ch_out=128)
-        self.Up_conv3 = conv_block(ch_in=256, ch_out=128)
+        self.Up_conv3 = ConvBlock(ch_in=256, ch_out=128)
 
         self.Up2 = up_conv(ch_in=128, ch_out=64)
-        self.Up_conv2 = conv_block(ch_in=128, ch_out=64)
+        self.Up_conv2 = ConvBlock(ch_in=128, ch_out=64)
 
         self.Conv_1x1 = nn.Conv2d(64, num_classes, kernel_size=1, stride=1, padding=0)
 
@@ -206,6 +316,51 @@ class U_Net(nn.Module):
         # outputs.append(d1)
         # return outputs
         return d1
+    
+
+class UNet_Transposed(nn.Module):
+    def __init__(self, in_chns, class_num):
+        super(UNet_Transposed, self).__init__()
+
+        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.Conv1 = ConvBlock(ch_in=in_chns, ch_out=64)
+        self.Conv2 = ConvBlock(ch_in=64, ch_out=128)
+        self.Conv3 = ConvBlock(ch_in=128, ch_out=256)
+        self.Conv4 = ConvBlock(ch_in=256, ch_out=512)
+        self.Conv5 = ConvBlock(ch_in=512, ch_out=1024)
+
+        params = {'in_chns': in_chns,
+                  #'feature_chns': [16, 32, 64, 128, 256],
+                  'feature_chns': [64, 128, 256, 512, 1024],
+                  #'dropout': [0.05, 0.1, 0.2, 0.3, 0.5],
+                  'class_num': class_num,
+                  'bilinear': False,
+                  'acti_func': 'relu'}
+        #self.encoder = Encoder(params)
+        self.decoder = Decoder(params)
+
+    def forward(self, x):
+        # encoding path
+        #feature = self.encoder(x)
+        x1 = self.Conv1(x)
+
+        x2 = self.Maxpool(x1)
+        x2 = self.Conv2(x2)
+
+        x3 = self.Maxpool(x2)
+        x3 = self.Conv3(x3)
+
+        x4 = self.Maxpool(x3)
+        x4 = self.Conv4(x4)
+
+        x5 = self.Maxpool(x4)
+        x5 = self.Conv5(x5)
+
+        #out = self.decoder(feature)
+        out = self.decoder([x1, x2, x3, x4, x5])
+
+        return out
 
 class R2U_Net(nn.Module):
     def __init__(self, in_channels=3, num_classes=1, t=2):
@@ -284,27 +439,27 @@ class AttU_Net(nn.Module):
 
         self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.Conv1 = conv_block(ch_in=in_channels, ch_out=64)
-        self.Conv2 = conv_block(ch_in=64, ch_out=128)
-        self.Conv3 = conv_block(ch_in=128, ch_out=256)
-        self.Conv4 = conv_block(ch_in=256, ch_out=512)
-        self.Conv5 = conv_block(ch_in=512, ch_out=1024)
+        self.Conv1 = ConvBlock(ch_in=in_channels, ch_out=64)
+        self.Conv2 = ConvBlock(ch_in=64, ch_out=128)
+        self.Conv3 = ConvBlock(ch_in=128, ch_out=256)
+        self.Conv4 = ConvBlock(ch_in=256, ch_out=512)
+        self.Conv5 = ConvBlock(ch_in=512, ch_out=1024)
 
         self.Up5 = up_conv(ch_in=1024, ch_out=512)
         self.Att5 = Attention_block(F_g=512, F_l=512, F_int=256)
-        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512)
+        self.Up_conv5 = ConvBlock(ch_in=1024, ch_out=512)
 
         self.Up4 = up_conv(ch_in=512, ch_out=256)
         self.Att4 = Attention_block(F_g=256, F_l=256, F_int=128)
-        self.Up_conv4 = conv_block(ch_in=512, ch_out=256)
+        self.Up_conv4 = ConvBlock(ch_in=512, ch_out=256)
 
         self.Up3 = up_conv(ch_in=256, ch_out=128)
         self.Att3 = Attention_block(F_g=128, F_l=128, F_int=64)
-        self.Up_conv3 = conv_block(ch_in=256, ch_out=128)
+        self.Up_conv3 = ConvBlock(ch_in=256, ch_out=128)
 
         self.Up2 = up_conv(ch_in=128, ch_out=64)
         self.Att2 = Attention_block(F_g=64, F_l=64, F_int=32)
-        self.Up_conv2 = conv_block(ch_in=128, ch_out=64)
+        self.Up_conv2 = ConvBlock(ch_in=128, ch_out=64)
 
         self.Conv_1x1 = nn.Conv2d(64, num_classes, kernel_size=1, stride=1, padding=0)
 
@@ -452,11 +607,11 @@ def r2_attention_unet(in_channels, num_classes):
     return model
 
 
-# if __name__ == '__main__':
-#     model = U_Net(1,10)
-#     model.eval()
-#     input = torch.rand(2,1,128,128)
-#     output = model(input)
-#     output = output[0].data.cpu().numpy()
-#     # print(output)
-#     print(output.shape)
+if __name__ == '__main__':
+    model = UNet_Transposed(3, 2)
+    model.eval()
+    input = torch.rand(2, 3, 128, 128)
+    output = model(input)
+    output = output[0].data.cpu().numpy()
+    # print(output)
+    print(output.shape)

9790a00dd77789551a1eaea73e7b9d886d83f371
luca-cnr <luca.ciampi@isti.cnr.it>
2024-04-24 15:36:03 +0200

    bugs fixed

--- models/getnetwork.py
+++ models/getnetwork.py
@@ -4,6 +4,7 @@ from models import *
 def get_network(network, in_channels, num_classes, **kwargs):
 
     # 2d networks
+    print(network)
     if network == 'unet':
         net = unet(in_channels, num_classes)
     elif network == 'unet_urpc':
@@ -12,7 +13,7 @@ def get_network(network, in_channels, num_classes, **kwargs):
         net = unet_cct(in_channels, num_classes)
 
     # 3d networks
-    if network == 'unet3d':
+    elif network == 'unet3d':
         net = unet3d(in_channels, num_classes)
     elif network == 'unet3d_min':
         net = unet3d_min(in_channels, num_classes)

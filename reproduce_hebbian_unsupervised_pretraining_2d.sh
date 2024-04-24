1bfbe5809fa71afae81ab41084a4189670cc43b1
luca-cnr <luca.ciampi@isti.cnr.it>
2024-04-24 15:40:58 +0200

    changes reproduce

--- reproduce_hebbian_unsupervised_pretraining_2d.sh
+++ reproduce_hebbian_unsupervised_pretraining_2d.sh
@@ -48,6 +48,8 @@ for K in ${K_VALUES[@]}; do
             python test_2d.py --dataset_name $DATASET --network unet --batch_size $EVAL_BATCH_SIZE --path_dataset $DATA_ROOT/$DATASET --best last --path_exp $EXP_ROOT/$DATASET/hebbian_unsup/unet_$HEBB_MODE/inv_temp-$K/regime-100/run-0 --hebbian_pretrain True --device $GPU --threshold $THRESHOLD
             python pretrain_hebbian_unsup_2d.py --dataset_name $DATASET --network unet_urpc --path_dataset $DATA_ROOT/$DATASET --path_root_exp $EXP_ROOT --batch_size $BATCH_SIZE --optimizer $OPTIMIZER --seed 0 --validate_iter $VALIDATE_ITER --device $GPU --lr $LR --loss dice --hebb_mode $HEBB_MODE --hebb_inv_temp $K --exclude $EXCLUDE_LAYER #--threshold $THRESHOLD
             python test_2d.py --dataset_name $DATASET --network unet_urpc --batch_size $EVAL_BATCH_SIZE --path_dataset $DATA_ROOT/$DATASET --best last --path_exp $EXP_ROOT/$DATASET/hebbian_unsup/unet_urpc_$HEBB_MODE/inv_temp-$K/regime-100/run-0 --hebbian_pretrain True --device $GPU --threshold $THRESHOLD      
+            python pretrain_hebbian_unsup_2d.py --dataset_name $DATASET --network unet_cct --path_dataset $DATA_ROOT/$DATASET --path_root_exp $EXP_ROOT --batch_size $BATCH_SIZE --optimizer $OPTIMIZER --seed 0 --validate_iter $VALIDATE_ITER --device $GPU --lr $LR --loss dice --hebb_mode $HEBB_MODE --hebb_inv_temp $K --exclude $EXCLUDE_LAYER #--threshold $THRESHOLD
+            python test_2d.py --dataset_name $DATASET --network unet_cct --batch_size $EVAL_BATCH_SIZE --path_dataset $DATA_ROOT/$DATASET --best last --path_exp $EXP_ROOT/$DATASET/hebbian_unsup/unet_cct_$HEBB_MODE/inv_temp-$K/regime-100/run-0 --hebbian_pretrain True --device $GPU --threshold $THRESHOLD         
         done
     done
 done

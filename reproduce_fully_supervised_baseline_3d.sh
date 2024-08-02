d3b53144aca6791383df1415898282ebfb73ed1b
luca-cnr <luca.ciampi@isti.cnr.it>
2024-08-02 16:44:55 +0200

    added support lits in script

--- reproduce_fully_supervised_baseline_3d.sh
+++ reproduce_fully_supervised_baseline_3d.sh
@@ -15,11 +15,12 @@ LR=0.1
 
 NETWORKS=(
     unet3d
-    vnet
+    #vnet
 )
 
 DATASETS=(
     Atrial
+    LiTS
 )
 
 DATA_ROOT=./data
@@ -31,8 +32,16 @@ EXP_ROOT=./runs
 for DATASET in ${DATASETS[@]}; do
     for NETWORK in ${NETWORKS[@]}; do
         for REP in $(seq $(( $START_REP )) $(( $REPS - 1 ))); do
-            python train_sup_3d.py --dataset_name $DATASET --network $NETWORK --path_dataset $DATA_ROOT/$DATASET --path_root_exp $EXP_ROOT --regime 100 --batch_size $BATCH_SIZE --optimizer $OPTIMIZER --seed $REP --validate_iter 2 --device $GPU --lr $LR --loss dice --patch_size "(96, 96, 80)"
-            python test_3d.py --dataset_name $DATASET --network $NETWORK --batch_size $EVAL_BATCH_SIZE --path_dataset $DATA_ROOT/$DATASET --best JI --path_exp $EXP_ROOT/$DATASET/fully_sup/$NETWORK/inv_temp-1/regime-100/run-$REP --patch_size "(96, 96, 80)" --patch_overlap "(48, 48, 20)" --device $GPU
+            case $DATASET in
+                Atrial)
+                    python train_sup_3d.py --dataset_name $DATASET --network $NETWORK --path_dataset $DATA_ROOT/$DATASET --path_root_exp $EXP_ROOT --regime 100 --batch_size $BATCH_SIZE --optimizer $OPTIMIZER --seed $REP --validate_iter 2 --device $GPU --lr $LR --loss dice --patch_size "(96, 96, 80)"
+                    python test_3d.py --dataset_name $DATASET --network $NETWORK --batch_size $EVAL_BATCH_SIZE --path_dataset $DATA_ROOT/$DATASET --best JI --path_exp $EXP_ROOT/$DATASET/fully_sup/$NETWORK/inv_temp-1/regime-100/run-$REP --patch_size "(96, 96, 80)" --patch_overlap "(48, 48, 40)" --device $GPU
+                    ;;
+                LiTS)
+                    python train_sup_3d.py --dataset_name $DATASET --network $NETWORK --path_dataset $DATA_ROOT/$DATASET --path_root_exp $EXP_ROOT --regime 100 --batch_size $BATCH_SIZE --optimizer $OPTIMIZER --seed $REP --validate_iter 2 --device $GPU --lr $LR --loss dice --patch_size "(112, 112, 32)"
+                    python test_3d.py --dataset_name $DATASET --network $NETWORK --batch_size $EVAL_BATCH_SIZE --path_dataset $DATA_ROOT/$DATASET --best JI --path_exp $EXP_ROOT/$DATASET/fully_sup/$NETWORK/inv_temp-1/regime-100/run-$REP --patch_size "(112, 112, 32)" --patch_overlap "(56, 56, 16)" --device $GPU
+                    ;;            
+            esac
         done
     done
 done
